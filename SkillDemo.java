b218bdd5a8cfa6c38f2a4636916e2b7925311101
Char15Xu <81684416+Char15Xu@users.noreply.github.com>
2022-04-30 23:54:56 -0700

    Added new test file and defined a method

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,5 @@
 public class SkillDemo{
-
+    public static int division(int a, int b){
+        return a/b;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(1, SkillDemo.division(2, 1));
+    }
+}
